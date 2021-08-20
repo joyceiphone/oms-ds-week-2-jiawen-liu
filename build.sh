ff3e2ebfd2b66e38ee00334158f5c371783a27a1
Joyce Liu <joyce@Joyces-MBP.fios-router.home>
2021-08-20 19:55:54 -0400

    add docker file

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 echo "run sender"
-cd /server/
-docker build -t my_ipc_server
+cd /server
+
+docker build -t my_ipc_server .
 docker images
 
-docker run -rm -p 12345:12345 my_ipc_server
+docker run -p 12345:12345 my_ipc_server
 
 echo "run client"
-cd /client/
-docker build -t my_ipc_client
+cd /client
+docker build -t my_ipc_client .
 docker images
 
 docker run  --network=host --name ipc_server_dns_name my_ipc_server
@@ -17,5 +18,5 @@ docker run  --network=host my_ipc_client
 docker network create my_socket_ipc_network
 
 docker run --network=my_socket_ipc_network --name ipc_server_dns_name my_ipc_server
-docerk run -rm --network-my_socket_ipc_network my_ipc_client
+docker run --network=my_socket_ipc_network my_ipc_client
 
